8b93adb4a56d9f02db9160d82fbc1c74756cd7b0
brandonsaiz <brandon.as.writer@gmail.com>
2025-09-08 20:54:20 -0800

    basics player one movement

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -11,11 +11,9 @@ var game_board_spots : Array
 var p_one_turn : bool = true
 var roll_complete : bool = false
 var roll_ready : bool = true
-
 var p_one_spot : int = 0
 var p_two_spot : int = 0
 var result : int
-
 
 func _ready() -> void:
 	dice.roll_complete.connect(_roll_complete)
@@ -29,27 +27,44 @@ func _input(_event) -> void:
 		roll_ready = false
 
 func _process(_delta) -> void:
+	if roll_complete:
+		roll_complete = false
+		
+#		result = 9
+		print(str(result))
+		move_player()
+		
+func move_player():
 	var current_player
-	var current_spot
+	var spaces = 0
+	
 	if p_one_turn: 
 		current_player = player_one
-		current_spot = p_one_spot
+		token_offset = -1 * abs(token_offset)
+		if p_one_spot + result > game_board_spots.size()-1:
+			print("fuuuuck")
+			return
 	else: 
-		current_player = player_two	
-		current_spot = p_two_spot
-	if roll_complete:
-		move_player(current_player, current_spot)
-		
-		
-func move_player(player : Sprite2D, spot : int):		
-	var tween = create_tween()
-	tween.tween_property(player, "global_position",\
-			game_board_spots[spot + result].global_position, 1.0)
-	await tween.finished
-	roll_ready = true
+		current_player = player_two
+		token_offset = abs(token_offset)
+		if p_two_spot + result > game_board_spots.size()-1:
+			print("fuuuuck")
+			return
+	while spaces != result:
+		spaces += 1
+		var next_spot
+		if p_one_turn:
+			p_one_spot += 1
+			next_spot = p_one_spot
+		else:
+			p_two_spot += 1
+			next_spot = p_two_spot
+		var tween = create_tween()
+		tween.tween_property(current_player, "global_position",\
+				game_board_spots[next_spot].global_position + token_offset, 0.8)
+		await tween.finished
 	
 func _roll_complete():
 	roll_complete = true
-	print(str(result))
 #	just used to test 
 #	roll_ready = true
