c08d8ce923e2df227c836f0756c9deba15b7b498
brandonsaiz <brandon.as.writer@gmail.com>
2025-09-27 14:31:17 -0800

    question box sounds

--- Scripts/question_box.gd
+++ Scripts/question_box.gd
@@ -4,10 +4,19 @@ class_name QuestionBox
 @onready var hbox = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer
 @onready var question = $PanelContainer/MarginContainer/VBoxContainer/Question
 @onready var close_timer = $CloseTimer
+@onready var sound_player = $questionSoundPlayer
+
+var start = preload("res://sounds/board game question comes up.wav")
+var right = preload("res://sounds/board game question right.wav")
+var wrong = preload("res://sounds/board game question wrong.wav")
 
 @export var left_button_correct : bool
 
 signal answered(correct : bool)
+
+func _ready():
+	sound_player.stream = start
+	sound_player.play()
 
 func _on_left_button_pressed():
 	emit_signal("answered", left_button_correct)
@@ -22,8 +31,13 @@ func _on_right_button_pressed():
 
 func _on_answered(correct):
 	hbox.visible = false
-	if correct: question.text = "That's correct!"
-	else: question.text = "Wrong!"
+	if correct: 
+		question.text = "That's correct!"
+		sound_player.stream = right
+	else: 
+		question.text = "Wrong!"
+		sound_player.stream = wrong
+	sound_player.play()
 	close_timer.start()
 	
 func _on_close_timer_timeout():
