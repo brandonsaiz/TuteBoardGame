fc65e2c79785310c31d326255122eec62962474c
brandonsaiz <brandon.as.writer@gmail.com>
2025-09-27 17:29:36 -0800

    dice noises!

--- Scripts/Dice.gd
+++ Scripts/Dice.gd
@@ -1,8 +1,9 @@
 class_name Dice
-extends AnimatedSprite2D
+extends Sprite2D
 
 @onready var roll_timer = $rollTimer
 @onready var roll_camera = $rollCamera
+@onready var animation_player = $AnimationPlayer
 
 var result : int
 
@@ -19,11 +20,11 @@ func _ready() -> void:
 func roll() -> int:
 	roll_timer.wait_time = randf_range(1, 2)
 	roll_timer.start()
-	play("default")
+	animation_player.play("default")
 	result = randi_range(0, 5)
 	return result + 1
 
 func _on_roll_timer_timeout() -> void:
-	stop()
+	animation_player.stop()
 	frame = result
 	emit_signal("roll_complete")
